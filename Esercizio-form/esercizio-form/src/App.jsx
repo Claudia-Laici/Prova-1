52a5a7f4268bbed886d20b01c61d2f1f0bcea774
Cla14 <127415926+Cla14@users.noreply.github.com>
2025-06-19 10:45:29 +0200

    esercizio form

---
+++ Esercizio-form/esercizio-form/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import StringStateExample from './components/form'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      <StringStateExample />
+      </div>
+   
+    </>
+  )
+}
+
+export default App
